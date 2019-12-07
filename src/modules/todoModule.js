fc1ef39794285ac6a9855d0c08f410003d100014
Seunghwan <seunghwansohn@gmail.com>
2019-12-07 14:52:09 +0700

    handleActions를 통해 리듀서 가독성 높임

--- src/modules/todoModule.js
+++ src/modules/todoModule.js
@@ -1,14 +1,19 @@
+import {createAction, handleActions} from 'redux-actions'
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'
 const INSERT = 'todos/INSERT'
 const TOGGLE = 'todos/TOGGLE'
 const REMOVE = 'todos/REMOVE'
 
-export const changeInput = input => (
-    console.log(input),
-    {
-    type: CHANGE_INPUT,
-    input
-})
+// export const changeInput = input => (
+//     {
+//     type: CHANGE_INPUT,
+//     input
+// })
+
+export const changeInput = createAction(CHANGE_INPUT, input => input)
+
+
 
 let id = 3;
 
@@ -47,33 +52,28 @@ const initialState = {
     ]
 }
 
-function todos (state = initialState, action){
-    switch (action.type){
-        case CHANGE_INPUT :
-            return {
-                ...state,
-                input: action.input
-            }
-        case INSERT :
-            return {
-                ...state,
-                todos : state.todoModule.concat(action.todo)
-            }
-        case TOGGLE : 
-            return {
-                ...state,
-                todos: state.todoModule.map(todo =>
-                    todo.id === action.id ? {...todo, done: !todo.done}: todo)
-            }
-        case REMOVE:
-            return {
-                ...state,
-                todos : state.todoModule.filter(todo => todo.id !== action.id)
-            }
-        default :
-        return state;    
+
+// function todos (state = initialState, action){
+//     switch (action.type){
+//         case CHANGE_INPUT :
+//             return {
+//                 ...state,
+//                 input: action.input
+//             }
+//         default :
+//         return state;    
         
-    }
-}
+//     }
+// }
+
+//위의 코드를 handleActions를 이용해 CHANGE_INPUT 타입부분과 바꾸면 이렇게 됨.
+//hanedleActions에서는 받아오는 값 부분이 모두 action.payload로 받는다는 점에 유의
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, action) => ({...state, input : action.payload}),
+    },
+    initialState,
+)
+
 
 export default todos;
